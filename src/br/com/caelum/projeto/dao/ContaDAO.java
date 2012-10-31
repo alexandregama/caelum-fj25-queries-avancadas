6925c935d795c71222b0e69e55a5943595cf8075
Alexandre Gama <alexandre.gama.lima@gmail.com>
2012-10-31 08:15:29 -0200

    Testes simples para retorno de uma Conta

--- src/br/com/caelum/projeto/dao/ContaDAO.java
+++ src/br/com/caelum/projeto/dao/ContaDAO.java
@@ -3,14 +3,18 @@ package br.com.caelum.projeto.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.projeto.modelo.Conta;
 
 public class ContaDAO {
 
 	private DAO<Conta> dao;
+	private EntityManager entityManager;
 	
 	public ContaDAO(EntityManager entityManager) {
+		this.entityManager = entityManager;
 		this.dao = new DAO<Conta>(entityManager, Conta.class);
 	}
 
@@ -26,4 +30,25 @@ public class ContaDAO {
 		this.dao.remove(conta);
 	}
 
+	public Conta buscaPorTitular_PositionParameter(String titular) {
+		Query query = entityManager.createQuery("select c from Conta c where c.titular = ?1");
+		query.setParameter(1, titular);
+		
+		return (Conta) query.getSingleResult();
+	}
+
+	public Conta buscaPorTitular_NamedParameter(String titular) {
+		Query query = entityManager.createQuery("select c from Conta c where c.titular = :titular");
+		query.setParameter("titular", titular);
+		
+		return (Conta) query.getSingleResult();
+	}
+	
+	public Conta buscaPorTitular_TypedQuery(String titular) {
+		TypedQuery<Conta> query = entityManager.createQuery("select c from Conta c where c.titular = :titular", Conta.class);
+		query.setParameter("titular", titular);
+		
+		return query.getSingleResult();
+	}
+	
 }
--- tests/br/com/caelum/projeto/dao/teste/ContaDAOTeste.java
+++ tests/br/com/caelum/projeto/dao/teste/ContaDAOTeste.java
@@ -1,8 +1,18 @@
 package br.com.caelum.projeto.dao.teste;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.projeto.dao.ContaDAO;
+import br.com.caelum.projeto.modelo.Conta;
 
 public class ContaDAOTeste {
 
@@ -10,8 +20,43 @@ public class ContaDAOTeste {
 	
 	private EntityManagerFactory factory;
 	
+	@Before
 	public void setUp() {
 		factory = Persistence.createEntityManagerFactory("controlefinancas");
 		entityManager = factory.createEntityManager();
 	}
+	
+	@Test
+	public void deveriaRetornarTodasAsContasCadastradas() throws Exception {
+		ContaDAO contaDAO = new ContaDAO(entityManager);
+		List<Conta> contas = contaDAO.lista();
+		
+		assertNotNull(contas);
+		assertEquals(3, contas.size());
+	}
+	
+	@Test
+	public void deveriaRetornarAContaDeDeterminadoTitular_UsandoPositionParameter() throws Exception {
+		ContaDAO contaDAO = new ContaDAO(entityManager);
+		Conta conta = contaDAO.buscaPorTitular_PositionParameter("Tiririca");
+		
+		assertEquals(conta.getTitular(), "Tiririca");
+	}
+	
+	@Test
+	public void deveriaRetornarAContaDeDeterminadoTitular_UsandoNamedParameter() throws Exception {
+		ContaDAO contaDAO = new ContaDAO(entityManager);
+		Conta conta = contaDAO.buscaPorTitular_NamedParameter("Tiririca");
+		
+		assertEquals(conta.getTitular(), "Tiririca");
+	}
+	
+	@Test
+	public void deveriaRetornarAContaDeDeterminadoTitular_UsandoTypedQuery() throws Exception {
+		ContaDAO contaDAO = new ContaDAO(entityManager);
+		Conta conta = contaDAO.buscaPorTitular_TypedQuery("Tiririca");
+		
+		assertEquals(conta.getTitular(), "Tiririca");
+	}
+	
 }
